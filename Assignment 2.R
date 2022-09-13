117557e6f9f0d184a3cba4941d9a68085bb560e0
Leigh <lam724@lehigh.edu>
2022-09-13 19:16:53 -0400

    Finished Assignment 2

--- Assignment 2.R
+++ Assignment 2.R
@@ -1,4 +1,5 @@
 #Assignment 2 Leigh McDermott
+
 library(dplyr)
 library(here)
 library(tidyverse)
@@ -9,3 +10,15 @@ Pens <- filter(State, state== "Pennsylvania")
 
 n <- length(Pens$date)
 Pens$incr_cases <- 1
+
+for (i in 2:n) {
+  Pens$incr_cases[i] <- (Pens$cases[i]-Pens$cases[i-1])
+}
+
+Pens$incr_deaths <- 0
+
+for (i in 2:n) {
+  Pens$incr_deaths[i] <- (Pens$deaths[i]-Pens$deaths[i-1]) 
+}
+
+checker <- sd(Pens$incr_cases,na.rm = FALSE)

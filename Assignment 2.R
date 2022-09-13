a728cfc0b3970feb3ca0e74c8fcb7033276f738e
Leigh <lam724@lehigh.edu>
2022-09-13 19:10:06 -0400

    More A2

--- Assignment 2.R
+++ Assignment 2.R
@@ -1,3 +1,11 @@
-library(dpylr)
+#Assignment 2 Leigh McDermott
+library(dplyr)
 library(here)
 library(tidyverse)
+
+State <- read_csv(here("covid-19-data","us-states.csv")) 
+
+Pens <- filter(State, state== "Pennsylvania")
+
+n <- length(Pens$date)
+Pens$incr_cases <- 1
